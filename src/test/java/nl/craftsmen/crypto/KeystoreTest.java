2b3db39cae163f5c50fafb0e19f1777dd7d7bbb0
Michel Schudel <michel.schudel@gmail.com>
2019-09-08 16:04:36 +0200

    renamed stuff to javazone

--- src/test/java/nl/craftsmen/crypto/KeystoreTest.java
+++ src/test/java/nl/craftsmen/crypto/KeystoreTest.java
@@ -23,18 +23,19 @@ import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.*;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
+import java.util.*;
 
 public class KeystoreTest {
 
 
     @Test
     public void createKeyStore() throws GeneralSecurityException, IOException, OperatorCreationException {
-        Files.delete(Path.of("myStore.pkcs12"));
+        if (Files.exists(Path.of("myStore.pkcs12"))) {
+            Files.delete(Path.of("myStore.pkcs12"));
+        }
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
         keyStore.load(null, null);
         //create keypair
@@ -45,7 +46,16 @@ public class KeystoreTest {
         Key pvt = kp.getPrivate();
         X509Certificate x509Certificate = generateRootCert(kp);
         keyStore.setCertificateEntry("myRootCertificate", x509Certificate);
+        Certificate[] chain = {x509Certificate};
+        keyStore.setKeyEntry("myPrivateKey", pvt, "password".toCharArray(), chain);
         keyStore.store(new FileOutputStream("myStore.pkcs12"), "password".toCharArray());
+        Enumeration<String> enums = keyStore.aliases();
+        System.out.println("aliases:");
+        enums.asIterator().forEachRemaining(System.out::println);
+        Key key = keyStore.getKey("myPrivateKey", "password".toCharArray());
+        System.out.println(key.toString());
+        Certificate certificate = keyStore.getCertificate("myRootCertificate");
+        System.out.println(certificate.toString());
     }
 
     public X509Certificate generateRootCert(KeyPair pair) throws IOException, OperatorCreationException, CertificateException {
--- src/test/java/nl/craftsmen/crypto/WalletEncryptionDemo.java
+++ src/test/java/nl/craftsmen/crypto/WalletEncryptionDemo.java
@@ -7,22 +7,18 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
 public class WalletEncryptionDemo {
 
     public static void main(String[] args) throws Exception {
         Utils.loadProvider();
-        String password = "password";
 
-        int keyLength = 256;
-        int saltLength = keyLength / 8; // It's bytes, not bits.
-        int iterations = 65536;
-        byte[] salt = new SecureRandom().generateSeed(saltLength);
-        PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, iterations, keyLength);
+        Key passwordKey = getPasswordKey("myPassword");
 
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256", "BC");
-        Key passwordKey = secretKeyFactory.generateSecret(keySpec);
 
         byte[] wallet = "{\"myAccountBalance\": 500}".getBytes();
         Utils.printText("Decrypted wallet: ", wallet);
@@ -34,10 +30,22 @@ public class WalletEncryptionDemo {
 
         Cipher decryptingCipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
         byte[] iv = encryptingCipher.getIV();
+
         decryptingCipher.init(Cipher.DECRYPT_MODE, passwordKey, new IvParameterSpec(iv));
-        byte[] plainText = decryptingCipher.doFinal(cipherText);
-        Utils.printText("Decrypted wallet: ", plainText);
+        byte[] plainText2 = decryptingCipher.doFinal(cipherText);
+        Utils.printText("Decrypted wallet: ", plainText2);
 
 
+    }
+
+    private static Key getPasswordKey(String password) throws InvalidKeySpecException, NoSuchProviderException, NoSuchAlgorithmException {
+        int keyLength = 256;
+        int saltLength = keyLength / 8; // It's bytes, not bits.
+        int iterations = 65536;
+        byte[] salt = new SecureRandom().generateSeed(saltLength);
+        PBEKeySpec passwordKeySpec = new PBEKeySpec(password.toCharArray(), salt, iterations, keyLength);
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256", "BC");
+        Key passwordKey = secretKeyFactory.generateSecret(passwordKeySpec);
+        return passwordKey;
     }
 }
